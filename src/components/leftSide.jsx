1247bddb6d8bac6d0f12c703d93939f420a4b79f
Shiham123 <s36651786@gmail.com>
2023-10-03 09:43:06 +0600

    right navbar complete

--- src/components/leftSide.jsx
+++ src/components/leftSide.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const LeftSideNav = () => {
-  return <div>this is left side nav</div>;
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch('/categories.json')
+      .then((response) => response.json())
+      .then((data) => setCategories(data));
+  }, []);
+
+  return (
+    <div className="space-y-4">
+      <h2 className="text-2xl">All Categories</h2>
+      {categories.map((category) => {
+        const { id, name } = category;
+        return (
+          <Link
+            className="block ml-4 text-xl font-semibold"
+            key={id}
+            to={`/category/${id}`}
+          >
+            {name}
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default LeftSideNav;
