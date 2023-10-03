09b5b89cb604d81532300bd12d19aaab3746ea9a
Shiham123 <s36651786@gmail.com>
2023-10-03 12:13:35 +0600

    ALHAMDULLILAH MALIK category show based on category data

--- src/components/leftSide.jsx
+++ src/components/leftSide.jsx
@@ -3,11 +3,19 @@ import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
   const [categories, setCategories] = useState([]);
+  const [newsData, setNewsData] = useState([]);
 
   useEffect(() => {
     fetch('/categories.json')
       .then((response) => response.json())
       .then((data) => setCategories(data));
+  }, []);
+
+  useEffect(() => {
+    fetch('/news.json')
+      .then((response) => response.json())
+      .then((data) => setNewsData(data))
+      .catch((error) => console.log(error));
   }, []);
 
   return (
@@ -25,6 +33,26 @@ const LeftSideNav = () => {
           </Link>
         );
       })}
+      <div className="pt-12">
+        {newsData.slice(0, 4).map((news) => {
+          const { _id, title, category_id, image_url } = news;
+          const matchCategory = categories.find(
+            (category) => category.id == category_id
+          );
+
+          return (
+            <div key={_id}>
+              <h1 className="text-[20px] font-semibold text-black py-8">
+                {title}
+              </h1>
+              <img src={image_url} alt="" />
+              <p className="text-[16px] font-medium py-4">
+                {matchCategory && <span>{matchCategory.name}</span>}
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
