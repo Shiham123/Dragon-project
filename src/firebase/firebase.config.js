29d4248de0c7c0c157b5f844e8d0aeb5f3931a69
Shiham123 <s36651786@gmail.com>
2023-10-04 19:35:19 +0600

    all complete

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBGZ5Wu_Bx9LGEn3XWu03zhhnyQD90IzUc',
-  authDomain: 'dragon-project-93949.firebaseapp.com',
-  projectId: 'dragon-project-93949',
-  storageBucket: 'dragon-project-93949.appspot.com',
-  messagingSenderId: '637941661537',
-  appId: '1:637941661537:web:30db6f3ab16afcf7914f20',
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 const app = initializeApp(firebaseConfig);
